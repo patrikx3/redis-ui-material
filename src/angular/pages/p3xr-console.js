6a2cdca158036ea9cb9658462c87e3bf28fe36ed
patrikx3 <alabard@gmail.com>
2018-12-23 20:36:27 +0100

    robo8x [chore]   12/23/2018, 8:36:27 PM

--- src/angular/pages/p3xr-console.js
+++ src/angular/pages/p3xr-console.js
@@ -34,7 +34,7 @@ p3xr.ng.component('p3xrConsole', {
             const windowHeight = $window.height()
             //console.log(windowHeight, minus)
 
-            const outputHeight = Math.max(windowHeight - minus - 50, 100)
+            const outputHeight = Math.max(windowHeight - minus - 32 , 0)
             $container.height(outputHeight)
             $container.css('max-height', `${outputHeight}px`)
 
