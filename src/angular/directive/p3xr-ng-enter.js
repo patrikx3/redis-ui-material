aed4fd7baaea759d9814999290169dba269c4e68
patrikx3 <alabard@gmail.com>
2019-01-01 11:58:23 +0100

    robo8x [chore]   1/1/2019, 11:58:23 AM

---
+++ src/angular/directive/p3xr-ng-enter.js
@@ -0,0 +1,12 @@
+p3xr.ng.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if (event.which === 13) {
+                scope.$apply(function () {
+                    scope.$eval(attrs.ngEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
+})
