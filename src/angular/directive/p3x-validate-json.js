58e5a407828a61dd7c74fff63f495f40bf003fdf
patrikx3 <alabard@gmail.com>
2021-02-13 09:06:23 +0100

    r0b08x [chore]   2/13/2021, 9:06:23 AM

--- src/angular/directive/p3x-validate-json.js
+++ src/angular/directive/p3x-validate-json.js
@@ -1,41 +1,52 @@
-p3xr.ng.directive('p3xValidateJson', function () {
+p3xr.ng.directive('p3xValidateJson', function ($timeout, $rootScope) {
     return {
         restrict: 'A',
         require: 'ngModel',
         scope: {
-            p3xValidateJsonRequired: '<'
+            p3xValidateJsonRequired: '<',
+            model : '=ngModel',
         },
         link: function (scope, elm, attrs, ngModel) {
 
             let required = scope.p3xValidateJsonRequired || false
             let globalValue
 
+            const setValidate = (isValid) => {
+                ngModel.$setValidity('p3xValidateJson', isValid);
+            }
+
             scope.$watch('p3xValidateJsonRequired', (val, oldVal) => {
                 required = val;
+                //console.warn('p3x-validate-json p3xValidateJsonRequired', 'required', required)
                 if (!required) {
-                    ngModel.$setValidity('p3xValidateJson', true);
+//                    ngModel.$modelValue = globalValue
+                    setValidate(true)
+
+                    scope.model = globalValue
+
                 } else {
                     try {
                         JSON.parse(globalValue)
-                        ngModel.$setValidity('p3xValidateJson', true);
+                        setValidate(true)
                     } catch (e) {
-                        ngModel.$setValidity('p3xValidateJson', false);
+                        setValidate(false)
                     }
                 }
             })
 
-            ngModel.$validators.p3xIsJson = (modelValue, viewValue) => {
+            ngModel.$validators.p3xValidateJson = (modelValue, viewValue) => {
                 globalValue = modelValue
+                //console.warn('p3x-validate-json p3xIsJson', 'modelValue', modelValue, 'viewValue', viewValue, 'required', required)
                 if (!required) {
-                    ngModel.$setValidity('p3xValidateJson', true);
-                    return true
+                    setValidate(true)
+                    return modelValue
                 }
                 try {
                     JSON.parse(modelValue)
-                    ngModel.$setValidity('p3xValidateJson', true);
-                    return true
+                    setValidate(true)
+                    return modelValue
                 } catch (e) {
-                    ngModel.$setValidity('p3xValidateJson', false);
+                    setValidate(false)
                     return false
                 }
             };
