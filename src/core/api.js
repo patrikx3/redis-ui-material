b5a6253374596810275daddcd41f036dda63e26f
patrikx3 <alabard@gmail.com>
2021-06-10 08:08:02 +0200

    r0b08x [chore]   6/10/2021, 8:08:02 AM

--- src/core/api.js
+++ src/core/api.js
@@ -3,7 +3,7 @@ global.p3xr.api = {
 }
 
 const apiUrl = new URL(location.toString())
-if (apiUrl.port === "8080") {
+if (global.p3xrDevMode === true && apiUrl.port === "8080") {
     global.p3xr.api.host = 'http://localhost:7843'
 } else {
     global.p3xr.api.host = `${apiUrl.protocol}//${apiUrl.host}`
